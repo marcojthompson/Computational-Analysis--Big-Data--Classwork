faaded5eb36d88b51941074568945f9d10c972cd
Marco Thompson <63751033+marcojthompson@users.noreply.github.com>
2022-04-05 19:10:34 +0200

    Started Week 8

---
+++ exercises/week8/MVWordCounter.py
@@ -0,0 +1,12 @@
+#Answer to 8.1.1.1-3
+from mrjob.job import MRJob
+class MRWordCounter(MRJob):
+
+	def mapper(self, _, line):
+		for word in line.split():
+			yield word, 1
+	def reducer(self, key, values):
+		yield key, sum(values)
+
+if __name__ == '__main__':
+	MRWordCounter.run()
